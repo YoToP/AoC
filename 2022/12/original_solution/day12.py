ab3d5fbd78577c44b622c945925c73b1c30f9c32
YoToP <yordie.me@tuta.io>
2022-12-19 21:19:51 +0100

    day 12 2022

--- 2022/12/original_solution/day12.py
+++ 2022/12/original_solution/day12.py
@@ -1,36 +1,42 @@
 from time import time
-from collections import defaultdict
-from copy import copy,deepcopy
-
-class Node:
-    def __init__(self, data):
-        self.nodes = []
-        self.data = data
-        self.stepsToEnd = 1e7
-    
-    def __repr__(self):
-        return f'Node("{self.data}","{self.nodes})'
-
-    def addNode(self,node):
-        self.nodes.append(node)
-
-    def validateNodes(self):
-        for node in self.nodes:
-            delta = node.data - self.data
-            if delta > 1 or delta < 0:
-                self.nodes.remove(node)
-        for node in self.nodes:
-            node.validateNodes()
-
-    def calcStepsToEnd(self,steps):
-        if steps < self.stepsToEnd:
-            self.stepsToEnd = steps
-            for node in self.nodes:
-                node.calcStepsToEnd(steps+1)
+from collections import defaultdict, deque
 
 
+def ShortestPathByBFS(g, source):
+    INFINITE = int(1e8)
+    UNDEFINED = -1
+    distances = defaultdict(lambda: INFINITE)
+    predecessors = defaultdict(lambda: UNDEFINED)
 
-        
+    distances[source] = 0
+
+    visited = defaultdict(lambda: False)
+    visited[source] = True
+
+    queue = deque()
+    queue.append(source)
+
+    while len(queue) > 0:
+        u = queue.popleft()
+
+        for v in g[u]:
+            if visited[v] != True:
+                visited[v] = True
+                distances[v] = distances[u] + 1
+                predecessors[v] = u
+                queue.append(v)
+
+    return distances
+
+
+def minVertex(queue, distance):
+    INFINITE = int(1e8)
+    UNDEFINED = -1
+    for v in queue:
+        if minDistance > distance[v]:
+            minDistance = distance[v]
+            minVertex = v
+    return minVertex
 
 
 def part1(path):
@@ -39,9 +45,11 @@ def part1(path):
     matrix = []
     height = 0
     width = 0
+    startI = 0
+    startJ = 0
     endI = 0
     endJ = 0
-    for line in lines: # load playing field into matrix
+    for line in lines:  # load playing field into matrix
         height += 1
         line = line.replace('\n', '')
         width = len(line)
@@ -49,55 +57,106 @@ def part1(path):
         for nr in line:
             if nr == 'S':
                 lstLine.append(0)
+                startI = height-1
+                startJ = len(lstLine)-1
             elif nr == 'E':
                 endI = height-1
-                lstLine.append(-1)
-                endJ = len(lstLine)
+                lstLine.append(ord(('z'))-ord('a')+2)
+                endJ = len(lstLine)-1
             else:
                 lstLine.append(ord((nr))-ord('a')+1)
         matrix.append(lstLine)
-    copymatrix = deepcopy(matrix)
-    head = Node(0)
-    nodeList = dict()
-    i = 0 #height
-    j = 0 #width
-    lastRowFirstNode = head
-    currentNode = head
-    for i in range(0,height):
-        for j in range(0,width):
-            if i == 0:
-                if j == 0:
-                    matrix[i][j] = head
-                else:
-                    newNode = Node(matrix[i][j])
-                    matrix[i][j-1].addNode(newNode)
-                    matrix[i][j] = newNode
-            else:
-                if j == 0:
-                    newNode = Node(matrix[i][j])
-                    matrix[i-1][j].addNode(newNode)
-                    matrix[i][j] = newNode
-                else:
-                    if matrix[i][j] == 9:
-                        pass
-                    newNode = Node(matrix[i][j])
-                    matrix[i][j-1].addNode(newNode)
-                    matrix[i-1][j].addNode(newNode)
-                    matrix[i][j] = newNode
-    head.validateNodes()
-    
-    head.calcStepsToEnd(0)
-    
-        
-    return matrix[endI][endJ].stepsToEnd
 
-if __name__ == '__main__': # Run when this script is not imported by another script(e.g. Unittest)
+    # create Adjeceny list
+    #(0,0), [(0,1),(1,0)]
+    AList = defaultdict(lambda: [])
+    for i in range(0, height):
+        for j in range(0, width):
+            _currentValue = matrix[i][j]
+            if i > 0:
+                if matrix[i-1][j] - _currentValue < 2:
+                    AList[(i, j)].append((i-1, j))
+            if i < height-1:
+                if matrix[i+1][j] - _currentValue < 2:
+                    AList[(i, j)].append((i+1, j))
+            if j < width-1:
+                if matrix[i][j+1] - _currentValue < 2:
+                    AList[(i, j)].append((i, j+1))
+            if j > 0:
+                if matrix[i][j-1] - _currentValue < 2:
+                    AList[(i, j)].append((i, j-1))
+
+    out = ShortestPathByBFS(AList, (startI, startJ))[(endI, endJ)]
+
+    return out
+
+
+def part2(path):
+    with open(path) as f:
+        lines = f.readlines()
+    matrix = []
+    height = 0
+    width = 0
+    endI = 0
+    endJ = 0
+    StartingPointsList = []
+    for line in lines:  # load playing field into matrix
+        height += 1
+        line = line.replace('\n', '')
+        width = len(line)
+        lstLine = []
+        for nr in line:
+            if nr == 'S':
+                lstLine.append(0)
+                StartingPointsList.append((height-1, len(lstLine)-1))
+            elif nr == 'E':
+                endI = height-1
+                lstLine.append(ord(('z'))-ord('a')+2)
+                endJ = len(lstLine)-1
+            else:
+                lstLine.append(ord((nr))-ord('a')+1)
+                if nr == 'a':
+                    StartingPointsList.append((height-1, len(lstLine)-1))
+        matrix.append(lstLine)
+
+    # create Adjeceny list
+    #(0,0), [(0,1),(1,0)]
+    AList = defaultdict(lambda: [])
+    for i in range(0, height):
+        for j in range(0, width):
+            _currentValue = matrix[i][j]
+            if i > 0:
+                if matrix[i-1][j] - _currentValue < 2:
+                    AList[(i, j)].append((i-1, j))
+            if i < height-1:
+                if matrix[i+1][j] - _currentValue < 2:
+                    AList[(i, j)].append((i+1, j))
+            if j < width-1:
+                if matrix[i][j+1] - _currentValue < 2:
+                    AList[(i, j)].append((i, j+1))
+            if j > 0:
+                if matrix[i][j-1] - _currentValue < 2:
+                    AList[(i, j)].append((i, j-1))
+    lowest = 1000
+    for sp in StartingPointsList:
+        out = ShortestPathByBFS(AList, sp)[(endI, endJ)]
+        if out < lowest:
+            lowest = out
+
+    return lowest
+
+
+# Run when this script is not imported by another script(e.g. Unittest)
+if __name__ == '__main__':
     start_time = int(round(time() * 1000))
     p1time = start_time
-    path = "2022/12/inputs/example.txt"
-    print("part 1:",part1(path))
-    print("### run time p1 is %s miliseconds" % (int(round(time() * 1000)) - p1time))
+    path = "2022/12/inputs/input.txt"
+    print("part 1:", part1(path))
+    print("### run time p1 is %s miliseconds" %
+          (int(round(time() * 1000)) - p1time))
     p2time = int(round(time() * 1000))
-    #print("part 2:",solvep2("2022/12/inputs/input.txt",5))
-    print("### run time p1 is %s miliseconds" % (int(round(time() * 1000)) - p2time))
-    print("### total run time is %s miliseconds" % (int(round(time() * 1000)) - start_time))
+    print("part 2:", part2("2022/12/inputs/input.txt"))
+    print("### run time p2 is %s miliseconds" %
+          (int(round(time() * 1000)) - p2time))
+    print("### total run time is %s miliseconds" %
+          (int(round(time() * 1000)) - start_time))
