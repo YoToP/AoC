44890eb5058a14a61ba6dc380d2be065b33deb8b
YoToP <yordie.me@tuta.io>
2023-05-07 17:50:11 +0200

    2022/11 delete scratch file

--- 2022/11/original_solution/scratch.py
+++
@@ -1,9 +0,0 @@
-
-for x in range(53,1007,19):
-    checkValue = (x-5+11)
-    if checkValue %17 == 10:
-        checkValue = checkValue - 8
-        if checkValue % 11 == 4:
-            print(x)
-            break
-exit()
