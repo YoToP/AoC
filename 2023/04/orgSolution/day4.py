397420bcee33bbcd990b57ab52bdb4c76eb7711d
YoToP <y.santana.sanz@gmail.com>
2023-12-04 15:43:15 +0100

    cleanup day4 2023

--- 2023/04/orgSolution/day4.py
+++ 2023/04/orgSolution/day4.py
@@ -30,35 +30,31 @@ def p1():
 def p2(): 
     with open("2023/04/inputs/input.txt") as f:
         lines = f.readlines()
-    score = 0
-    AMOUNT_WIN_NUMBERS = 10
-    def InitDict():
-        return 1
-    cardCounterDict = defaultdict(InitDict)
+    cardCounterDict = defaultdict(lambda: 1)
     cardnr = 0
     for line in lines:
-        line = line.strip()
-        _, numbers = line.split(":")
+        _, numbers = line.strip().split(":")
         cardnr += 1
         winning, mynumbers = numbers.split(" | ")
         winset = set()
         for winNr in winning.split(" "):
             if winNr.isdigit():
                 winset.add(int(winNr))
-        AMOUNT_WIN_NUMBERS = len(winset)
-        amount_play_numbers = 0
+        amountWinningNumbers = len(winset)
+        amountPlayNumbers = 0
         for winNr in mynumbers.split(" "):
             if winNr.isdigit():
                 winset.add(int(winNr))
-                amount_play_numbers += 1
+                amountPlayNumbers += 1
 
-        if len(winset) < AMOUNT_WIN_NUMBERS+amount_play_numbers:
-            amountWinning = AMOUNT_WIN_NUMBERS+amount_play_numbers-len(winset)
+        if len(winset) < amountWinningNumbers+amountPlayNumbers: #only calc when at least 1 number is a winning number
+            amountWinning = amountWinningNumbers+amountPlayNumbers-len(winset)
             for i in range(1,amountWinning+1):
                 cardCounterDict[i+cardnr] += 1 * cardCounterDict[cardnr]
         else:
-            pass
-        cardCounterDict[cardnr] = cardCounterDict[cardnr]
+            cardCounterDict[cardnr] #make sure to add the last one to the Dict
+        
+    score = 0
     for k,v in cardCounterDict.items():
         score+= v
     return score
