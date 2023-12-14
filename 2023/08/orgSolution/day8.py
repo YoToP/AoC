bacd438e6a851dce9b1894a22244c137c9c717b6
YoToP <y.santana.sanz@gmail.com>
2023-12-14 17:38:09 +0100

    day 8 2023 part1 done, part 2 naive

---
+++ 2023/08/orgSolution/day8.py
@@ -0,0 +1,77 @@
+from time import time
+from collections import defaultdict, deque
+
+
+def p1():
+    with open("2023/08/inputs/input.txt") as f:
+        lines = f.read()
+
+    steps, nodes = lines.split('\n\n')
+    nodes = nodes.split('\n')
+    puzzleMap = dict()
+    for node in nodes:
+        source, dest = node.split(' = ')
+        left, right = dest.split(', ')
+        left = left.replace('(','')
+        right = right.replace(')','')
+        puzzleMap[source] = (left,right)
+    currentPos = 'AAA'
+    stepCount = 0
+    while currentPos != 'ZZZ':
+        for step in steps:
+            if step == 'L':
+                currentPos = puzzleMap[currentPos][0]
+            else:
+                currentPos = puzzleMap[currentPos][1]
+            stepCount += 1
+            if currentPos == 'ZZZ':
+                break
+    return stepCount
+
+
+def p2():
+    with open("2023/08/inputs/input.txt") as f:
+        lines = f.read()
+    steps, nodes = lines.split('\n\n')
+    nodes = nodes.split('\n')
+    puzzleMap = dict()
+    currentPositions = []
+    for node in nodes:
+        source, dest = node.split(' = ')
+        left, right = dest.split(', ')
+        left = left.replace('(','')
+        right = right.replace(')','')
+        puzzleMap[source] = (left,right)
+        if source[2] == 'A':
+            currentPositions.append(source) # fill with startpositions
+    
+
+    stepCount = 0
+    allEndwithZ = False
+    while not allEndwithZ:
+        for step in steps:
+            for i in range(0,len(currentPositions)):
+                if step == 'L':
+                    currentPositions[i] = puzzleMap[currentPositions[i]][0]
+                else:
+                    currentPositions[i] = puzzleMap[currentPositions[i]][1]
+            stepCount += 1
+            AllZ = True
+            for currentPos in currentPositions:
+                if currentPos[2] != 'Z':
+                    AllZ = False
+                    break # break from check
+            if AllZ:
+                allEndwithZ = AllZ
+                break
+    return stepCount
+
+if __name__ == '__main__':
+    start_time = int(round(time() * 1000))
+    print('part 1:', p1())
+    print("### p1 run time is %s miliseconds" %
+          (int(round(time() * 1000)) - start_time))
+    start_time = int(round(time() * 1000))
+    print('part 2:', p2())
+    print("### p2 run time is %s miliseconds" %
+          (int(round(time() * 1000)) - start_time))
