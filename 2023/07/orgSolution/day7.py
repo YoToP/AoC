f038d63285be47a445b7b84eff70ca97a987f497
YoToP <y.santana.sanz@gmail.com>
2023-12-14 20:42:18 +0100

    day 7 2023

--- 2023/07/orgSolution/day7.py
+++ 2023/07/orgSolution/day7.py
@@ -1,101 +1,44 @@
 from time import time
-from collections import defaultdict, deque
-# A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
-cardValues = {
-  "A": 14,
-  "K": 13,
-  "Q": 12,
-  "J": 11,
-  "T": 10,
-  "9": 9,
-  "8": 8,
-  "7": 7,
-  "6": 6,
-  "5": 5,
-  "4": 4,
-  "3": 3,
-  "2": 2
-}
+from collections import Counter
+
+def replaceCards(cards):
+    cards = cards.replace('T',chr(ord('9')+1))
+    cards = cards.replace('J',chr(ord('9')+2))
+    cards = cards.replace('Q',chr(ord('9')+3))
+    cards = cards.replace('K',chr(ord('9')+4))
+    cards = cards.replace('A',chr(ord('9')+5))
+    return cards
+
 def p1():
-    with open("2023/07/inputs/example.txt") as f:
+    with open("2023/07/inputs/input.txt") as f:
         lines = f.read().split('\n')
 
-    highCardList = deque()
-    onePairList = deque()
-    twoPairList = deque()
-    theeKindList = deque()
-    fourKindList = deque()
-    FiveKindList = deque()
+    strength = []
     for line in lines:
         cards, bid = line.split(' ')
-        handData = []
-        handData.append(cards)
-        handData.append(int(bid))
-        cardSet = defaultdict(int)
-        for card in cards:
-            cardSet[card] += 1
+        cardSet = Counter(cards)
+        a = list(cardSet.values())
+
         match len(cardSet):
             case 1: #Five of a kind
-                FiveKindList.append(handData)
+                strength.append((((5,replaceCards(cards)),bid)))
             case 2: #four of a kind
-                fourKindList.append(handData)
+                strength.append((((4,replaceCards(cards)),bid)))
             case 3: #three of a kind, or two pair
-                highestV = 0
-                for k,v, in cardSet.items():
-                    if v > highestV:
-                        highestV = v
-                if highestV == 2:
-                    twoPairList.append(handData)
-                elif highestV == 3:
-                    theeKindList.append(handData)
-                else:
-                    print(f"Computer says no: {v}")
+                if sorted(a, reverse=True)[0] == 2:
+                    strength.append((((2,replaceCards(cards)),bid)))
+                if sorted(a, reverse=True)[0] == 3:
+                    strength.append((((3,replaceCards(cards)),bid)))
             case 4: #one pair
-                onePairList.append(handData)
+                strength.append((((1,replaceCards(cards)),bid)))
             case 5: #high cards/ all diferent
-                highCardList.append(handData)
-    pass
-    rank = 1
+                strength.append((((0,replaceCards(cards)),bid)))
+    sortedList = sorted(strength, key=lambda strength:strength[0])
+
     score = 0
-    #first lowest
-    for hand in highCardList:
-        rank += 1
-
-    #one pair
-    if len(onePairList) == 1:
-        score += onePairList[0][1] * rank
-    elif len(onePairList) > 1:
-        pass
-
-    #two pair
-    if len(twoPairList) == 1:
-        score += twoPairList[0][1] * rank
-    elif len(twoPairList) > 1:
-        i = 0
-        highestCard = 0
-        while i < len(twoPairList[0][0]):
-            cardDict = defaultdict(int)
-            for decks in twoPairList:
-                if cardValues[twoPairList[0][0][i]] >highestCard:
-                    highestCard = cardValues[twoPairList[0][0][i]]
-                cardDict[cardValues[decks[0][i]]] += 1
-            i+=1
-        pass
-            
-        i+=1
-    
-    #three kind
-    for hand in theeKindList:
-        rank += 1
-
-    #four kind
-    for hand in fourKindList:
-        rank += 1
-
-    #five kind
-    for hand in FiveKindList:
-        rank += 1
-    return lowest
+    for i,(_,bid) in enumerate(sortedList):
+        score += (i+1)*int(bid)
+    return score
 
 
 def p2(): 
