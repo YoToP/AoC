4c8918ec3d6973eda962b804ff44be63ba1317b9
YoToP <y.santana.sanz@gmail.com>
2024-11-30 14:46:10 +0100

    finish day7

--- 2023/07/orgSolution/day7.py
+++ 2023/07/orgSolution/day7.py
@@ -9,6 +9,14 @@ def replaceCards(cards):
     cards = cards.replace('A',chr(ord('9')+5))
     return cards
 
+def replaceCardsP2(cards):
+    cards = cards.replace('T',chr(ord('9')+1))
+    cards = cards.replace('J','0')
+    cards = cards.replace('Q',chr(ord('9')+3))
+    cards = cards.replace('K',chr(ord('9')+4))
+    cards = cards.replace('A',chr(ord('9')+5))
+    return cards
+
 def p1():
     with open("2023/07/inputs/input.txt") as f:
         lines = f.read().split('\n')
@@ -45,8 +53,61 @@ def p1():
 
 
 def p2(): 
-    
-    return 0
+    with open("2023/07/inputs/input.txt") as f:
+        lines = f.read().split('\n')
+
+    strength = []
+    for line in lines:
+        cards, bid = line.split(' ')
+        cards = replaceCardsP2(cards)
+        cardSet = Counter(cards)
+
+        # sort the deck to have the highest count card fist, and replace all J with this card.
+        if '0' in cardSet:
+            #check for amounts in the counter
+            maxValue = 0
+            keyList = []
+            for k,v in cardSet.items():
+                if k == '0': # Exclude J
+                    continue
+                if v > maxValue:
+                    maxValue = v
+                    keyList.clear()
+                    keyList.append(k)
+                elif v == maxValue:
+                    keyList.append(k)
+            if len(keyList) > 0: # edge case if all cards are J, then the list is empty
+                highKey = max(keyList)
+                cardSet[highKey] += cardSet['0']
+                cardSet.pop('0')
+        a = list(cardSet.values())
+            
+        match len(cardSet):
+            case 1: #Five of a kind
+                strength.append((((6,cards),bid)))
+            case 2: #four of a kind or full house
+                if sorted(a, reverse=True)[0] == 4:
+                    strength.append((((5,cards),bid)))
+                elif sorted(a, reverse=True)[0] == 3:
+                    strength.append((((4,cards),bid)))    
+                else:
+                    pass            
+            case 3: #three of a kind, or two pair
+                if sorted(a, reverse=True)[0] == 2:
+                    strength.append((((2,cards),bid)))
+                elif sorted(a, reverse=True)[0] == 3:
+                    strength.append((((3,cards),bid)))
+                else:
+                    pass
+            case 4: #one pair
+                strength.append((((1,cards),bid)))
+            case 5: #high cards/ all diferent
+                strength.append((((0,cards),bid)))
+    sortedList = sorted(strength, key=lambda b:b[0])
+    score = 0
+    for i,(_,bid) in enumerate(sortedList,1):
+        score += i*int(bid)
+    return score
 
 if __name__ == '__main__':
     start_time = int(round(time() * 1000))
