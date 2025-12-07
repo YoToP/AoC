a417026eabc8bd8b55c4ae07680307860e1a63c4
YoToP <y.santana.sanz@gmail.com>
2025-12-07 22:28:49 +0100

    p2 finished

--- 2025/02/solution.py
+++ 2025/02/solution.py
@@ -13,38 +13,25 @@ def isValidP1(s:str) -> bool:
 
 def isValidP2(s:str) -> bool:
     stringSize = len(s)
-
-    if stringSize % 2 > 0: #odd values only needs singles to be checked
-        allSame = True
-        firstChar = s[0]
-        for c in s:
-            if c != firstChar:
-                allSame = False
-        if allSame:
-            return False
-        else:
-            return True
-    else:
-        maxX = int(stringSize/2)
-        x = 1
-        while x <= maxX:
-            if stringSize % x == 0:
-                firstSlice = s[0:x]
-                isSame = True
-                for i in range(x,int(stringSize/x+x),x):
-                    nextSlice = s[i:i+x]
-                    if firstSlice != nextSlice:
-                        isSame = False
-                        break #break this for loop
-                if isSame:
-                    return False
-            x += 1
-        return True
-    return False
+    maxX = int(stringSize/2)
+    x = 1
+    while x <= maxX:
+        if stringSize % x == 0:
+            firstSlice = s[0:x]
+            isSame = True
+            for i in range(x,int(stringSize/x*x),x):
+                nextSlice = s[i:i+x]
+                if firstSlice != nextSlice:
+                    isSame = False
+                    break #break this for loop
+            if isSame:
+                return False
+        x += 1
+    return True
 
 def puzzle():
     ranges = []
-    with open("2025/02/example.txt") as f:
+    with open("2025/02/input.txt") as f:
         ranges = f.readline().strip().split(",")
     intranges = []
     for r in ranges:
@@ -56,7 +43,7 @@ def puzzle():
         for _nr in range(_s,_e+1):
             if not isValidP1(str(_nr)):
                 scoreP1 += _nr
-            if _nr == 824824824:
+            if _nr == 1000:
                 pass
             if not isValidP2(str(_nr)):
                 scoreP2 += _nr
