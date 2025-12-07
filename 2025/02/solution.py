e7caafce264db4b7627362b69b8cab300464e025
YoToP <y.santana.sanz@gmail.com>
2025-12-07 17:54:28 +0100

    refactor to use same reading code, and making the difference in isValid functions

--- 2025/02/solution.py
+++ 2025/02/solution.py
@@ -1,18 +1,33 @@
 from collections import defaultdict
 
-def p1():
-    def isValid(s:str) -> bool:
-        stringSize = len(s)
 
-        if stringSize % 2 > 0: #odd values do not need to be checked
-            return True
+def isValidP1(s:str) -> bool:
+    stringSize = len(s)
+
+    if stringSize % 2 > 0: #odd values do not need to be checked
+        return True
+    else:
+        x = int(stringSize/2)
+        if s[:x] == s[x:stringSize]:
+            return False
         else:
-            x = int(stringSize/2)
-            if s[:x] == s[x:stringSize]:
-                return False
-            else:
-                return True
-        return False
+            return True
+    return False
+
+def isValidP2(s:str) -> bool:
+    stringSize = len(s)
+
+    if stringSize % 2 > 0: #odd values do not need to be checked
+        return True
+    else:
+        x = int(stringSize/2)
+        if s[:x] == s[x:stringSize]:
+            return False
+        else:
+            return True
+    return False
+
+def puzzle():
     ranges = []
     with open("2025/02/input.txt") as f:
         ranges = f.readline().strip().split(",")
@@ -20,17 +35,20 @@ def p1():
     for r in ranges:
         start,end = r.split("-")
         intranges.append((int(start),int(end)))
-    score = 0
+    scoreP1 = 0
+    scoreP2 = 0
     for _s,_e in intranges:
         for _nr in range(_s,_e+1):
-            if not isValid(str(_nr)):
-                score += _nr
-    return score
+            if not isValidP1(str(_nr)):
+                scoreP1 += _nr
+            if not isValidP2(str(_nr)):
+                scoreP2 += _nr
+    return (scoreP1,scoreP2)
 
 
-def p2():
-    return 0
+
 
 if __name__ == '__main__':
-    print(f"part 1: {p1()}")
-    print(f"part 2: {p2()}")
+    p1,p2 = puzzle()
+    print(f"part 1: {p1}")
+    print(f"part 2: {p2}")
