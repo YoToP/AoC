4e88a92dfd65e7db504a5574607a10c804e1442e
YoToP <y.santana.sanz@gmail.com>
2025-12-14 22:15:38 +0100

    2025 day 1 part, OK I WILL COUNT 1 BY 1

--- 2025/01/solution.py
+++ 2025/01/solution.py
@@ -23,26 +23,19 @@ def p2(steps = []):
     dial = 50
     amountZero = 0
     for step in steps:
-        startAtZero = False
-        if dial == 0:
-            startAtZero = True
         if step[0] == 'L':
-            dial -= int(step.replace('L',''))
+            for _i in range(int(step.replace('L',''))):
+                dial -= 1
+                if dial == 0:
+                    amountZero += 1
+                elif dial == -1:
+                    dial = 99
         elif step[0] == 'R':
-            dial += int(step.replace('R',''))
-        if dial < 0:
-            if startAtZero:
-                amountZero -= 1
-                startAtZero = False
-            while dial < 0:
-                dial += 100
-                amountZero += 1
-        elif dial > 99:
-            while dial > 99:
-                dial -= 100
-                amountZero += 1
-        elif dial == 0:
-            amountZero += 1
+            for _i in range(int(step.replace('R',''))):
+                dial += 1
+                if dial == 100:
+                    amountZero += 1
+                    dial = 0
     return amountZero
 
 if __name__ == '__main__':
