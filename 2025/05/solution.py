5f049b90bd4878ee4b9cf6faa47c1e0550f59ee4
YoToP <y.santana.sanz@gmail.com>
2025-12-16 22:36:41 +0100

    2025 day 5 part 2

--- 2025/05/solution.py
+++ 2025/05/solution.py
@@ -16,12 +16,39 @@ def p1(ranges = [],idList = []):
     return score
 
 def p2(ranges = []):
-    ids = set()
+    RangeList = []
+    score = 0
     for _r in ranges:
         _s,_e = _r.split("-")
-        for i in range(int(_s),int(_e)+1):
-            ids.add(i)
-    return len(ids)
+        RangeList.append((int(_s),int(_e)))
+    while True:
+        if len(RangeList) == 0:
+            break
+        lowestStart = int(1e100)
+        lowestRange = None
+        lowestEnd = 0
+        for _r in RangeList:
+            _s,_e = _r
+            if _s < lowestStart:
+                lowestStart = _s
+                lowestRange = _r
+        lowestEnd = lowestRange[1]
+        RangeList.remove(lowestRange)
+
+        while True:
+            removed = False
+            for _r in RangeList:
+                _s,_e = _r
+                if lowestEnd >= _s:
+                    if lowestEnd < _e:
+                        lowestEnd = _e
+                    RangeList.remove(_r)
+                    removed = True
+                    break
+            if not removed:
+                break
+        score += (lowestEnd-lowestStart+1)
+    return score
 
 if __name__ == '__main__':
     with open("2025/05/input.txt") as f:
